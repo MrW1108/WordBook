47fbc6121f608884d6330541debfd1b246a3331a
MrW <1637011263@qq.com>
2020-09-20 14:32:04 +0800

    first commit

---
+++ app/src/main/java/com/example/wordbook/OperationDB.java
@@ -0,0 +1,82 @@
+package com.example.wordbook;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.wordbook.dummy.Words;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class OperationDB {
+
+    static WordsDBHelper helper;
+
+    static OperationDB ins = new OperationDB();
+    public static OperationDB getOperations(){
+        return OperationDB.ins;
+    }
+
+    private OperationDB(){
+        if(helper == null){
+            helper = new WordsDBHelper(WordsApplication.getContext());
+        }
+    }
+
+    public void close(){
+        if(helper != null){
+            helper.close();
+        }
+    }
+
+    public Words.WordDescription getSingleWord(String id){
+        Words.WordDescription result = new Words.WordDescription();
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql = "select * from words where _id = ?";
+        Cursor cursor = db.rawQuery(sql,new String[]{id});
+        if(cursor.moveToFirst()){
+            result.setWord(cursor.getString(cursor.getColumnIndex("word")));
+            result.setMeaning(cursor.getString(cursor.getColumnIndex("meaning")));
+            result.setSample(cursor.getString(cursor.getColumnIndex("sample")));
+            return result;
+        }
+        return null;
+
+    }
+
+    public ArrayList<Map<String,String>> getAllWord(){
+        SQLiteDatabase db = helper.getReadableDatabase();
+        String sql = "select * from words";
+        Cursor cursor = db.rawQuery(sql,null);
+        return ConvertCursorToWordList(cursor);
+    }
+
+    //前者为id，后者为Word
+    public ArrayList<Map<String,String>> ConvertCursorToWordList(Cursor cursor){
+        ArrayList<Map<String,String>> map = new ArrayList<Map<String, String>>();
+        if(cursor.moveToFirst()){
+            
+        }
+        return null;
+    }
+
+    public void InsertWord (String word, String meaning, String sample){}
+
+    public void Insert(String word, String meaning, String sample){}
+
+    public void DeleteWord (String strID){}
+    public void Delete(String strID){}
+
+    public void UpdateUseSql(String strId, String strWord, String strMeaning, String strSample) {}
+    public void Update(String strId, String strWord, String strMeaning, String strSample) { }
+    //查找
+    public ArrayList<Map<String, String>> SearchUseSql(String strWordSearch) {
+        return null;
+    }
+    public ArrayList<Map<String, String>> Search(String strWordSearch) {
+        return null;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/wordbook/WordsDBHelper.java
@@ -0,0 +1,40 @@
+package com.example.wordbook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.wordbook.dummy.Words;
+
+public class WordsDBHelper extends SQLiteOpenHelper {
+
+    private final static String DATABASE_NAME= "wordsdb";//数据库名字
+    private final static int DATABASE_VERSION = 1;//数据库版本
+    //建表语句
+    private final static String SQL_CREATE_DATABASE = "CREATE TABLE " + Words.Word.TABLE_NAME + " (" +
+            Words.Word._ID + " VARCHAR(32) PRIMARY KEY NOT NULL," +
+            Words.Word.COLUMN_NAME_WORD + " TEXT UNIQUE NOT NULL,"+
+            Words.Word.COLUMN_NAME_MEANING + " TEXT,"
+            + Words.Word.COLUMN_NAME_SAMPLE + " TEXT)";
+    //删表语句
+    private final static String SQL_DELETE_DATABASE = "DROP TABLE IF EXISTS " + Words.Word.TABLE_NAME;
+
+    public WordsDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    //建表
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_DATABASE);
+    }
+
+    @Override
+    //更新，先删后建
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DELETE_DATABASE);
+        onCreate(db);
+    }
+}
